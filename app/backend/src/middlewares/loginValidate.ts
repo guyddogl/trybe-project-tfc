ee3599ff23bcf73edd5412e37399fa43f98929d3
Guyddo <guyddogl@gmail.com>
2023-01-21 09:15:23 -0300

    Create loginValidate middleware

---
+++ app/backend/src/middlewares/loginValidate.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  const isValidEmail = email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+  if (!isValidEmail) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
+  next();
+};
+
+export default validateLogin;
