75186c61ca82b5e0a1771e5d8c1ffec782a3af17
Guyddo <guyddogl@gmail.com>
2023-01-21 11:37:52 -0300

    Create teams controller

---
+++ app/backend/src/controllers/teamsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import teamsService from '../services/teamsService';
+
+const getTeams = async (req: Request, res: Response):Promise<void> => {
+  const { status, message, teamsFound } = await teamsService.getTeams();
+
+  if (status === 200) {
+    res.status(status).json(teamsFound);
+  } else res.status(status).json({ message });
+};
+
+const findTeam = async (req: Request, res: Response):Promise<void> => {
+  const { id } = req.params;
+  const { status, message, teamFound } = await teamsService.findTeam(id);
+
+  if (status === 200) {
+    res.status(status).json(teamFound);
+  } else res.status(status).json({ message });
+};
+
+export default {
+  getTeams,
+  findTeam,
+};
