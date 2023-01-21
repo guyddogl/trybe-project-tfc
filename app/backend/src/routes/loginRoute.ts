50d1feade349060fd414d50da6bc5ed8cd55cfdf
Guyddo <guyddogl@gmail.com>
2023-01-21 09:14:59 -0300

    Add middleware loginValidate

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import loginValidate from '../middlewares/loginValidate';
+import usersController from '../controllers/usersController';
 
 const routers = Router();
 
-routers.post('/');
+routers.post('/', loginValidate, usersController.findUser);
+routers.get('/validate', loginValidate, usersController.findUser);
 
 export default routers;
