53419c2fa7bc9c5ecbcd5084e72faaf968619a8e
Guyddo <guyddogl@gmail.com>
2023-01-22 14:30:08 -0300

    Create leaderboard route

--- app/backend/src/routes/leaderboardRoute.ts
+++ app/backend/src/routes/leaderboardRoute.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import leaderboardController from '../controllers/leaderboardController';
+// import generalController from '../controllers/generalController';
 
 const routers = Router();
 
-routers.get('/:query', leaderboardController.getLeaderboard);
+routers.get('/home', leaderboardController.getLeaderboard);
+routers.get('/away', leaderboardController.getLeaderboard);
+routers.get('/', leaderboardController.getGeneralLeaderboard);
 
 export default routers;
