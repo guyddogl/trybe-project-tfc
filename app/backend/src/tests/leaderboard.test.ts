0776dfdc53d2055534ed8288faa277c92075dac8
Guyddo <guyddogl@gmail.com>
2023-01-22 11:41:49 -0300

    Create leaderboard tests

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,27 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes da rota leaderboard', () => {
+  afterEach(sinon.restore);
+
+  it('Retorna a classificação considerando home team', async () => {
+    const result = await chai.request(app).get('/leaderboard/home');
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.an('array');
+  });
+
+  it('Retorna a classificação considerando away team', async () => {
+    const result = await chai.request(app).get('/leaderboard/team');
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.an('array');
+  });
+
+});
